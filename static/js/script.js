8046a77809133f5246208bf213fd46f5ba7b42bb
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-08-04 18:15:58 +0200

    Make ctrl-clicking a row open in a new tab

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,10 @@
-$('tr[data-href]').click(function() {
-    window.location.href = this.getAttribute('data-href');
+$("tr[data-href]").click(function(e) {
+    var url = this.getAttribute("data-href");
+    var openInNew = e.ctrlKey || (e.metaKey && navigator.userAgent.indexOf("Mac OS X"));
+
+    if (openInNew) {
+        window.open(url, "_blank");
+    } else {
+        window.location.href = url;
+    }
 });
