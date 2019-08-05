cdccab20f8b4453636c5bda50f28b943841aacd1
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-08-05 22:46:42 +0200

    Fix CORS for firefox

--- academy/cors.py
+++ academy/cors.py
@@ -6,5 +6,5 @@ class CorsMiddleware:
         response = self.get_response(request)
         response["Access-Control-Allow-Origin"] = "*"
         response["Access-Control-Allow-Methods"] = "*"
-        response["Access-Control-Allow-Headers"] = "*"
+        response["Access-Control-Allow-Headers"] = "content-type, authorization"
         return response
