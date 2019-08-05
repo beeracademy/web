eeff3907c57faa977138423050cd87cfc9b8ed07
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-08-05 08:10:31 +0200

    Fix times in import_games

--- games/management/commands/import_games.py
+++ games/management/commands/import_games.py
@@ -18,12 +18,13 @@ class Command(BaseCommand):
         parser.add_argument("--games", action="store_true")
         parser.add_argument("--game-player-relations", action="store_true")
         parser.add_argument("--cards", action="store_true")
+        parser.add_argument("--fix-times", action="store_true")
         parser.add_argument("--chugs", action="store_true")
 
     def get_rows(self, table_name):
         with open(f"dump/tables/{table_name}.tsv", newline="") as f:
             reader = csv.DictReader(f, dialect="excel-tab")
-            return tqdm(list(reader))
+            return list(reader)
 
     def timestamp_seconds_to_datetime(self, timestamp):
         d = datetime.datetime.fromtimestamp(int(timestamp))
@@ -35,7 +36,7 @@ class Command(BaseCommand):
     def import_users(self):
         print("Importing users...")
         User.objects.all().delete()
-        for user in self.get_rows("user"):
+        for user in tqdm(self.get_rows("user")):
             User.objects.create(
                 id=user["id"],
                 username=user["username"],
@@ -58,7 +59,7 @@ class Command(BaseCommand):
     def import_games(self):
         print("Importing games...")
         Game.objects.all().delete()
-        for game in self.get_rows("game"):
+        for game in tqdm(self.get_rows("game")):
             Game.objects.create(
                 id=game["id"],
                 start_datetime=self.timestamp_milliseconds_to_datetime(
@@ -72,7 +73,10 @@ class Command(BaseCommand):
 
     def import_game_player_relations(self):
         print("Importing game-player relations...")
-        for relation in self.get_rows("playergamerelation"):
+        relations = sorted(
+            self.get_rows("playergamerelation"), key=lambda r: int(r["place"])
+        )
+        for relation in tqdm(relations):
             game = Game.objects.get(id=relation["gameid"])
             user = User.objects.get(id=relation["profileid"])
             game.players.add(user)
@@ -85,9 +89,17 @@ class Command(BaseCommand):
 
     def create_card(self, card_id, game, drawn_datetime):
         value, suit = self.get_value_and_suit(card_id)
+
         Card.objects.create(
             game=game, value=value, suit=suit, drawn_datetime=drawn_datetime
         )
+
+    def is_bad_card_date(self, dt):
+        # Every card drawn on this date,
+        # actually has an unknown drawn datetime
+        BAD_CARD_DATE = (2015, 7, 5)
+
+        return dt.timetuple()[:3] == BAD_CARD_DATE
 
     def import_cards(self):
         print("Importing cards...")
@@ -97,15 +109,58 @@ class Command(BaseCommand):
             r["id"]: r for r in self.get_rows("playergamerelation")
         }
 
-        for relation in self.get_rows("gamecardrelation"):
+        relations = sorted(
+            self.get_rows("gamecardrelation"),
+            key=lambda relation: (
+                int(relation["roundid"]),
+                int(player_game_relations[relation["playergamerelation"]]["place"]),
+            ),
+        )
+
+        card_delta = {}
+
+        for relation in tqdm(relations):
             pg_relation = player_game_relations[relation["playergamerelation"]]
-            game = Game.objects.get(id=pg_relation["gameid"])
-            drawn_datetime = datetime.datetime.strptime(
-                relation["drawtime"], "%Y-%m-%d %H:%M:%S"
+            try:
+                game = Game.objects.get(id=pg_relation["gameid"])
+            except Game.DoesNotExist:
+                # This can happen if the game is bad
+                # and hasn't been imported
+                continue
+
+            # Actually not in UTC, but we will try to fix this.
+            drawn_datetime = pytz.utc.localize(
+                datetime.datetime.strptime(relation["drawtime"], "%Y-%m-%d %H:%M:%S")
             )
+
+            if game.id not in card_delta:
+                # First card in game
+                # Try to adjust it to start_datetime, which is in UTC
+                if game.start_datetime.year == 1970 or self.is_bad_card_date(
+                    drawn_datetime
+                ):
+                    card_delta[game.id] = datetime.timedelta()
+                else:
+                    diff_start = drawn_datetime - game.start_datetime
+                    seconds = diff_start.total_seconds()
+                    hours = seconds / (60 * 60)
+                    rounded_hours = round(hours)
+                    delta = datetime.timedelta(hours=rounded_hours)
+                    card_delta[game.id] = delta
+                    assert diff_start - delta >= datetime.timedelta(), (
+                        diff_start,
+                        game.id,
+                    )
+
+            drawn_datetime -= card_delta[game.id]
+
             try:
                 self.create_card(relation["cardid"], game, drawn_datetime)
             except IntegrityError:
+                # Game 1072 has a harmless duplicate card
+                if game.id == 1072:
+                    continue
+
                 print(
                     f"Bad game: {game.id} (duplicate cards: {self.get_value_and_suit(relation['cardid'])})"
                 )
@@ -121,11 +176,111 @@ class Command(BaseCommand):
                 )
                 game.delete()
 
+    def fix_times(self):
+        print("Fixing times of games/cards")
+        for game in tqdm(Game.objects.all()):
+            cards = game.ordered_cards()
+
+            if self.is_bad_card_date(cards.first().drawn_datetime):
+                # We assume that the first card has an unknown drawn datetime
+                # if and only if all of them have
+                continue
+
+            # We generally expect cards draw times to be ordered,
+            # but they don't necessarily lie in the interval of
+            # [game.start_datetime, game.end_datetime]
+            prev_dt = None
+            bad_card_order = False
+            for c in cards:
+                if prev_dt is not None and c.drawn_datetime < prev_dt:
+                    bad_card_order = True
+                    break
+                prev_dt = c.drawn_datetime
+
+            if game.start_datetime > game.end_datetime:
+                bad_card_order = True
+
+            first_card = cards.first()
+            last_card = cards.last()
+
+            card_duration = last_card.drawn_datetime - first_card.drawn_datetime
+            game_duration = game.get_duration()
+            if card_duration > game_duration:
+                print(card_duration, game_duration)
+                bad_card_order = True
+
+            # Manually fix for some specific games
+            if bad_card_order:
+                bad_card_order = False
+                if game.id == 1339:
+                    # A specially fucked up game
+                    print(f"Manually fixing {game.id}")
+
+                    game.end_datetime += datetime.timedelta(hours=2)
+                    game.save()
+
+                    after_dst = False
+                    prev_dt = None
+                    for c in cards:
+                        if prev_dt is not None and c.drawn_datetime < prev_dt:
+                            after_dst = True
+                        prev_dt = c.drawn_datetime
+
+                        if after_dst:
+                            c.drawn_datetime += datetime.timedelta(hours=2)
+                            c.save()
+                elif game.id == 1229:
+                    # I don't know what happened to this game,
+                    # but it isn't that hard to fix
+                    print(f"Manually fixing {game.id}")
+
+                    cards_l = list(cards)
+                    cards_l[7].drawn_datetime += datetime.timedelta(seconds=15)
+                    cards_l[7].save()
+                    cards_l[8].drawn_datetime += datetime.timedelta(seconds=15)
+                    cards_l[8].save()
+                elif game.id == 1294:
+                    # This was played while a DST transition happened
+                    print(f"Manually fixing {game.id}")
+
+                    game.save()
+
+                    after_dst = False
+                    prev_dt = None
+                    for c in cards:
+                        if (
+                            prev_dt is not None
+                            and c.drawn_datetime > prev_dt + datetime.timedelta(hours=1)
+                        ):
+                            after_dst = True
+                        prev_dt = c.drawn_datetime
+
+                        if after_dst:
+                            c.drawn_datetime -= datetime.timedelta(hours=1)
+                            c.save()
+
+            if (
+                first_card.drawn_datetime < game.start_datetime
+                or last_card.drawn_datetime > game.end_datetime
+            ):
+                bad_card_order = True
+
+            if bad_card_order:
+                print(f"== Game with bad card order found: {game.id} ==")
+                print("Start:", game.start_datetime)
+                print("End:", game.end_datetime)
+                print()
+
+                for c in cards:
+                    print(str(c.drawn_datetime.time()))
+
+                print()
+
     def import_chugs(self):
         print("Importing chugs...")
         Chug.objects.all().delete()
 
-        for chug in self.get_rows("chuck"):
+        for chug in tqdm(self.get_rows("chuck")):
             card_id = chug["cardid"]
             value, suit = self.get_value_and_suit(card_id)
             try:
@@ -165,6 +320,9 @@ class Command(BaseCommand):
 
         if options["all"] or options["cards"]:
             self.import_cards()
+
+        if options["all"] or options["fix_times"]:
+            self.fix_times()
 
         if options["all"] or options["chugs"]:
             self.import_chugs()
