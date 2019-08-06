fb74f01754e320c307e685f8403593277b47e583
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-08-06 03:05:42 +0200

    Add myself as ADMIN

--- academy/settings/production.py
+++ academy/settings/production.py
@@ -1,10 +1,11 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = [
-	"academy.beer"
-]
+ALLOWED_HOSTS = ["academy.beer"]
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 SESSION_COOKIE_SECURE = True
+
+# Server admins (get an email when server errors happen)
+ADMINS = [("Asger Hautop Drewsen", "asgerdrewsen@gmail.com")]
