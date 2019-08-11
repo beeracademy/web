a6d7ef3ac91a4a32162de5a8f48eee5303de891a
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-08-11 21:50:50 +0200

    Read email password from .env

--- academy/settings/production.py
+++ academy/settings/production.py
@@ -8,7 +8,7 @@ load_dotenv()
 DEBUG = False
 ALLOWED_HOSTS = ["academy.beer"]
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 SESSION_COOKIE_SECURE = True
 
@@ -16,6 +16,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "django.drewsen@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 
 SERVER_EMAIL = "no-reply@academy.beer"
 DEFAULT_FROM_EMAIL = "no-reply@academy.beer"
