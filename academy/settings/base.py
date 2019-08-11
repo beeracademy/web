2362a024b343614ff2932f589031a77f5bda9fab
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-08-11 20:42:43 +0200

    Only force admins to have strong passwords

--- academy/settings/base.py
+++ academy/settings/base.py
@@ -95,13 +95,9 @@ PASSWORD_HASHERS = [
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
+# Disable all validators, we want to support "test"
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
-    },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+    {"NAME": "academy.password_validators.StrongAdminPasswordValidator"}
 ]
 
 
