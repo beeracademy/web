f7e9e20efcfe401d20962b87c68ac23e235fdc88
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-08-12 04:36:59 +0200

    Add IMAGE_{WIDTH,HEIGHT} to template processor

--- academy/context_processors.py
+++ academy/context_processors.py
@@ -1,5 +1,7 @@
 from django.conf import settings
+from games.models import User
 
 
 def constants(request):
-    return {"PLAY_URL": settings.PLAY_URL}
+    width, height = User.IMAGE_SIZE
+    return {"PLAY_URL": settings.PLAY_URL, "IMAGE_WIDTH": width, "IMAGE_HEIGHT": height}
