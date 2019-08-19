35c8d518a4cd22866683aff75f13c507c57c861f
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-08-19 17:56:43 +0200

    Format player stats better

--- academy/settings/base.py
+++ academy/settings/base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     "django_extensions",
     "debug_toolbar",
     "rest_framework",
