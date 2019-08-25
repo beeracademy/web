112d1a1de738bd2cafd52107c9ff532f173b81f3
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-08-25 23:43:08 +0200

    Only allow CORS on api paths and from correct domain

--- academy/cors.py
+++ academy/cors.py
@@ -1,10 +1,22 @@
+from django.conf import settings
+
+
 class CorsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
-        response["Access-Control-Allow-Origin"] = "*"
-        response["Access-Control-Allow-Methods"] = "*"
-        response["Access-Control-Allow-Headers"] = "content-type, authorization"
+        if request.path.startswith("/api/") or request.path.startswith(
+            "/api-token-auth/"
+        ):
+            if settings.DEBUG:
+                origin = "*"
+            else:
+                origin = "https://game.academy.beer"
+
+            response["Access-Control-Allow-Origin"] = origin
+            response["Access-Control-Allow-Methods"] = "options, get, post"
+            response["Access-Control-Allow-Headers"] = "content-type, authorization"
+
         return response
