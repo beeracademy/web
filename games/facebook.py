741725e44c73c317b8816a9bd5f85fb2566c70ef
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-09-02 21:37:30 +0200

    Don't crash server when Facebook posting fails

--- games/facebook.py
+++ games/facebook.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from facebook import GraphAPI
+from facebook import GraphAPI, GraphAPIError
 
 
 def post_to_page(msg, link):
@@ -12,7 +12,12 @@ def post_to_page(msg, link):
         if hasattr(settings, "FACEBOOK_PAGE_ID") and hasattr(
             settings, "FACEBOOK_ACCESS_TOKEN"
         ):
-            graph = GraphAPI(settings.FACEBOOK_ACCESS_TOKEN)
-            graph.put_object(settings.FACEBOOK_PAGE_ID, "feed", message=msg, link=link)
+            try:
+                graph = GraphAPI(settings.FACEBOOK_ACCESS_TOKEN)
+                graph.put_object(
+                    settings.FACEBOOK_PAGE_ID, "feed", message=msg, link=link
+                )
+            except GraphAPIError as e:
+                print("Failed to post to facebook page:", e)
         else:
             print("WARNING: missing facebook page id or token.")
