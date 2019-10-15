283819f2f09c3307a608b30d30149fb57b3bf683
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2019-10-15 17:00:13 +0200

    Add HTML logging for server errors

--- academy/settings/production.py
+++ academy/settings/production.py
@@ -35,11 +35,39 @@ DATABASES = {
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Server admins (get an email when server errors happen)
-ADMINS = [
-	('Asger Hautop Drewsen', 'asgerdrewsen@gmail.com'),
-]
+ADMINS = [("Asger Hautop Drewsen", "asgerdrewsen@gmail.com")]
 
 FACEBOOK_PAGE_ID = "227174884109471"
 FACEBOOK_ACCESS_TOKEN = os.environ["FACEBOOK_ACCESS_TOKEN"]
 
 CELERY_BROKER_URL = "amqp://guest:guest@rabbitmq:5672//"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        # Include the default Django email handler for errors
+        # This is what you'd get without configuring logging at all.
+        "mail_admins": {
+            "class": "django.utils.log.AdminEmailHandler",
+            "level": "ERROR",
+            # But the emails are plain text by default - HTML is nicer
+            "include_html": True,
+        },
+        # Log to a text file that can be rotated by logrotate
+        "logfile": {
+            "class": "logging.handlers.WatchedFileHandler",
+            "filename": "/app/django.log",
+        },
+    },
+    "loggers": {
+        # Again, default Django configuration to email unhandled exceptions
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+        # Might as well log any errors anywhere else in Django
+        "django": {"handlers": ["logfile"], "level": "ERROR", "propagate": False},
+    },
+}
