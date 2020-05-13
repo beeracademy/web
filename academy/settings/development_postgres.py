e7fb939cfd6dfe7abc780be9b3a35a6c91bf5b0e
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2020-05-13 08:06:06 +0200

    Add development_postgres settings

---
+++ academy/settings/development_postgres.py
@@ -0,0 +1,11 @@
+from .development import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "HOST": "localhost",
+        "PORT": 5432,
+    }
+}
