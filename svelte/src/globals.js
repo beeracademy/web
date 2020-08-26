5132485749e3267a09a4bd927bd1001101b48d5d
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2020-08-26 14:14:11 +0200

    Better colors for players
    See
    https://github.com/matplotlib/matplotlib/pull/9255/files

--- svelte/src/globals.js
+++ svelte/src/globals.js
@@ -1,11 +1,9 @@
 export const card_constants = JSON.parse(document.getElementById("card_constants").textContent);
 export const userColors = [
-	"#16a085",
-	"#27ae60",
-	"#2980b9",
-	"#8e44ad",
-	"#2c3e50",
-	"#f39c12",
-	"#d35400",
-	"#c0392b",
+	'#006BA4',
+	'#FF800E',
+	'#ABABAB',
+	'#595959',
+	'#5F9ED1',
+	'#C85200',
 ];
