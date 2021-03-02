c5ace11408c7ef5db68d6aca4dac21f0fadd7cd9
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2021-03-02 23:44:16 +0100

    Run django.setup()

--- academy/asgi.py
+++ academy/asgi.py
@@ -5,6 +5,10 @@ defined in the ASGI_APPLICATION setting.
 
 import os
 
+import django
+
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
