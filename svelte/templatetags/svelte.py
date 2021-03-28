d5be5545cd4514b2863e09c222d5077d35d4d1e3
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2021-03-28 07:32:46 +0200

    Use vite instead of snowpack for svelte

--- svelte/templatetags/svelte.py
+++ svelte/templatetags/svelte.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag("svelte_include.html")
+@register.inclusion_tag("svelte_include_generated.html")
 def svelte_include(component):
-    return {"script": f"svelte/{component}_init.js"}
+    return {}
