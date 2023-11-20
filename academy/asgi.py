a51f709f952305233e42e346300450a6e2677a11
Asger Hautop Drewsen <asger@tyilo.com>
2023-11-20 20:00:50 +0100

    Fix startup error

--- academy/asgi.py
+++ academy/asgi.py
@@ -6,12 +6,13 @@ defined in the ASGI_APPLICATION setting.
 import os
 
 import django
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import chat.routing
 
 django.setup()
+
+from channels.auth import AuthMiddlewareStack  # noqa: E402
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
+from django.core.asgi import get_asgi_application  # noqa: E402
+import chat.routing  # noqa: E402
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "academy.settings.development")
