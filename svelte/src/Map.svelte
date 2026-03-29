ed727456928380894f6aedb53b98a339300673fe
Asger Hautop Drewsen <asger@tyilo.com>
2026-03-30 01:17:37 +0200

    Send Referer header to OSM

--- svelte/src/Map.svelte
+++ svelte/src/Map.svelte
@@ -15,6 +15,7 @@ onMount(() => {
 	L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 		attribution:
 			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		referrerPolicy: "strict-origin",
 	}).addTo(map);
 	L.marker([location.latitude, location.longitude])
 		.addTo(map)
